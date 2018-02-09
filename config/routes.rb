908129da98435eca7c4b488e4fd82043075eebb3
Elisa Mendoza <ElisaMendoza@Elisas-MacBook-Air.local>
2018-02-08 17:44:20 -0800

    Checkpoint 21 Topics and Posts: Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,8 @@
 Rails.application.routes.draw do
+  resources :topics do 
+    resources :posts, except: [:index]
+  end 
 
-  #1
-  resources :posts
-
-  #2
   get 'about' => 'welcome#about'
 
   root 'welcome#index'
